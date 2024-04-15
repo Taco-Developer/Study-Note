74ca4297f9e38bc9122618a50fb7d8ece2baa46a
Deokho Kim <demiacan@gmail.com>
2024-04-15 19:31:30 +0900

    Level 2

---
+++ CS/Algorithm/NodeJS/Programmers/다음 큰 숫자.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  // 2진수에서 1의 개수 반환 함수
+  function getOneCnt(n) {
+    let oneCnt = 0;
+
+    while (n > 0) {
+      const left = n % 2;
+      if (left === 1) oneCnt++;
+      n = Math.floor(n / 2);
+    }
+    return oneCnt;
+  }
+
+  const nOneCnt = getOneCnt(n);
+  for (let i = n + 1; i < n * 2; i++) {
+    const oneCnt = getOneCnt(i);
+    if (oneCnt === nOneCnt) return i;
+  }
+}
